48595d5a344baa1834011f9cdfc2791819783ace
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-04-28 08:24:17 +0000

    Use host networking for OLM image builds
    Pass `--network host` to the build command so the OLM image build can
    complete when the default container network fails to start.

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -10,6 +10,7 @@ registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
 
 "${CONTAINER_CLI}" build --pull \
+    --network host \
     --build-arg https_proxy=${https_proxy} \
     --volume /etc/yum.repos.d:/etc/yum.repos.d \
     --tag ${docker_tag} -f ./olm/builds/Dockerfile .
